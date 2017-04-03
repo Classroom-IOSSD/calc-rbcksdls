3cf4175421572faa3e36e4156ce0a454a81e5087
rbcksdls <rbcksdls@naver.com>
2017-04-03 21:23:36 +0900

    fix bugs

--- calc.c
+++ calc.c
@@ -5,29 +5,38 @@ int main(){
 	FILE *fp = NULL;
 	int operand1, operand2;
 	char operator = ' ';
-	int result, line = 0;
+	float result = 0;
+	int line = 0;
 
 	fp = fopen("read.txt","r");
 	if(fp!=NULL){
 		fscanf(fp, "%d", &line);
-	
+		
 		for(int i=0; i<line; i++) {
 			fscanf(fp, "%d %c %d",&operand1, &operator, &operand2);
+			if(operator == ' ') {
+				return 0;
+			}
 			switch(operator) {
 				case '+':
-				result = add(operand1, operator);
+				result = add(operand1, operand2);
 				break;
 				case '-':
-				result = minus(operand1, operator);
+				result = minus(operand1, operand2);
 				break;
 				case '*':
-				result = mul(operand1, operator);
+				result = mul(operand1, operand2);
+				break;
 				case '/':
-				result = div(operand1, operator);
+				result = div(operand1, operand2);
 				break;
 			}		
 			printf("%d %c %d = %d\n",
 				 operand1, operator, operand2, result);
+			operand1 = 0;
+			operator = ' ';
+			operand2 = 0;
+			result = 0;
 		}
 	}
 	return 0;
--- operators.c
+++ operators.c
@@ -1,16 +1,21 @@
 #include "operators.h"
 
-int add(int op1, int op2) {
+float add(float op1, float op2) {
 	return op1+op2;
 }
-int minus(int op1, int op2) {
+float minus(float op1, float op2) {
 	return op1-op2;
 }
-int mul(int op1, int op2) {
+float mul(float op1, float op2) {
 	return op1*op2;
 }
 
-int div(int op1, int op2) {
-	return op1%op2;
+float div(float op1, float op2) {
+	if (op2 != 0) {
+		return op1/op2;
+	}
+	else {
+		return -1;
+	}
 }
 
